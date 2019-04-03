24a66f38a46ded1bfe930994dc1a4de105d9704b
xiaoshi <xiaoshi@shantech.net>
2019-04-03 12:18:29 +0800

    增加轻量TREE导出

--- src/editor/editor/managers/ExportManager.js
+++ src/editor/editor/managers/ExportManager.js
@@ -128,6 +128,95 @@ b3e.editor.ExportManager = function(editor) {
     return data;
   };
 
+  this.exportLightTree = function (tree) {
+    var project = editor.project.get();
+    if (!project) return;
+
+    if (!tree) {
+      tree = project.trees.getSelected();
+    } else {
+      tree = project.trees.get(tree);
+      if (!tree) return;
+    }
+
+    var root = tree.blocks.getRoot();
+    var first = getBlockChildrenIds(root);
+    var data = {
+      root         : first[0] || null,
+      properties   : root.properties,
+      nodes        : {},
+    };
+
+    var replaceTreeIds = {};
+
+    tree.blocks.each(function(block) {
+      if (block.category === 'tree') {
+        var subTree = project.trees.get(block.name);
+        if (subTree) {
+          var subData = this.exportLightTree(subTree, true);
+          var subNodes = subData.nodes;
+          for (var sub in subNodes) {
+            data.nodes[sub] = subNodes[sub];
+          }
+          replaceTreeIds[block.id] = subData.root;
+        }
+      } else if (block.category !== 'root') {
+        var d ={
+          id          : block.id,
+          name        : block.name,
+          properties  : block.properties,
+        };
+
+        var children = getBlockChildrenIds(block);
+        if (block.category === 'composite') {
+          d.children = children;
+        } else if (block.category === 'decorator') {
+          d.child = children[0];
+        }
+
+        data.nodes[block.id] = d;
+      }
+    }.bind(this));
+
+    var allNodes = data.nodes;
+    for (var id in allNodes) {
+      var node = allNodes[id];
+      if (node.child) {
+        node.child = replaceTreeIds[node.child] || node.child;
+      } else if (node.children) {
+        var children = node.children;
+        for (var i = 0; i < children.length; i++) {
+          var origin = children[i];
+          children[i] = replaceTreeIds[origin] || origin;
+        }
+      }
+    }
+
+    return data;
+  };
+
+  this.nodesToData = function() {
+    var project = editor.project.get();
+    if (!project) return;
+
+    var data = [];
+    project.nodes.each(function(node) {
+      if (!node.isDefault) {
+        data.push({
+          version     : b3e.VERSION,
+          scope       : 'node',
+          name        : node.name,
+          category    : node.category,
+          title       : node.title,
+          description : node.description,
+          properties  : node.properties,
+        });
+      }
+    });
+
+    return data;
+  };
+
   this.nodesToJavascript = function() {};
 
   this._applySettings = function(settings) {};
